7e58dab828d54f1b8854babc16f8fce3026ee3bf
andresCamp <andreslcampos@icloud.com>
2024-04-17 13:30:49 -0700

    image added in

--- app/waitlist/page.tsx
+++ app/waitlist/page.tsx
@@ -12,45 +12,21 @@ import WaitlistForm from './WaitlistForm'
 
 const Page = () => {
     return (
-        <div className="flex flex-col-reverse sm:flex-row min-h-screen overflow-hidden">
+        <div className="flex flex-col-reverse sm:flex-row h-screen overflow-hidden">
 
             {/* Image Section */}
-            <div className="flex h-1/3 sm:h-full sm:w-1/2">
-                {/* Mobile nav */}
-                <div className='flex justify-between p-3 items-center absolute top-0 z-50 left-0 w-full sm:hidden'>
-                    {/* <h2 className='text-2xl w-1/3'>MyStory</h2> */}
-                    {/* <Link href={"/"} className='flex justify-center transition-all duration-500 hover:opacity-70  sm:hidden w-1/3'> */}
-                        <Image
-                            src={logo}
-                            alt="logo" 
-                            height={55}
-                        />
-                        
-                    {/* </Link> */}
-                    {/* <Link href="https://app.mystory.bio/sign-in" className='flex justify-end sm:hidden w-1/3'>
-                        <button className=' ring-2 ring-white text-white text-xl rounded-3xl px-5 py-1 transition-all duration-500 hover:bg-primary-100 hover:scale-105'>
-                            Sign In
-                        </button>
-                    </Link> */}
-                </div>
+            <div className="hidden sm:flex sm:h-full sm:w-1/2 relative">
+    <Image
+        src={img}
+        alt="MyStory image holding hands"
+        layout="fill"  // Ensures the image covers the available space
+        objectFit="cover"  // Adjust the fit of the image within the container
+    />
+</div>
 
-                {/* Main Image */}
-                <div className="w-full h-full relative ">
-                    <div className="md:h-full md:w-full flex items-end"> 
-                            <Image
-                                src={img}
-                                fill
-                                placeholder="blur"
-                                alt="MyStory image holding hands"
-                                className="w-full h-full object-cover object-bottom" 
-                            />
-                    </div>
-                </div>
-            </div>
-            
+
             {/* Text Section */}
-            <div className=" flex flex-1 flex-col items-center sm:items-start justify-center bg-secondary p-8 h-2/3 sm:h-auto sm:w-1/2">
-                
+            <div className="flex flex-1 flex-col items-center sm:items-start justify-center bg-secondary p-8 h-2/3 sm:h-auto sm:w-1/2">
                 {/* Logo */}
                 <Link href={"/"} className='transition-all duration-500 hover:opacity-70 hidden sm:block'>
                     <Image
@@ -59,38 +35,32 @@ const Page = () => {
                         height={65}
                     />
                 </Link>
-                {/* /Logo */}
-
                 {/* Title & Subtitle */}
                 <div className="w-full h-full max-w-md mx-auto sm:gap-10 gap-3 sm:pt-0 text-left flex flex-col sm:justify-center items-center sm:items-start">
                     <div className="text-gray-800 w-full px-4 py-5 sm:p-6">
                         <div className="text-left">
                             <h2 className="font-bold text-4xl sm:text-4xl lg:text-5xl mb-4 leading-tight">
-                            Join the Waitlist
+                                Join the Waitlist
                             </h2>  
                         </div>
                         <div>
                             <p className="text-lg mb-5">
-                            We&apos;re excited to invite you to be among the first to experience our new way of preserving personal histories. Sign up today and tell us why you&apos;re interested—we&apos;re eager to hear your story!
+                                We&apos;re excited to invite you to be among the first to experience our new way of preserving personal histories. Sign up today and tell us why you&apos;re interested—we&apos;re eager to hear your story!
                             </p>
                             <WaitlistForm />
                             <p className="text-sm text-gray-600 mt-4">
-                            We respect your privacy. Your information is secure and will never be shared.
+                                We respect your privacy. Your information is secure and will never be shared.
                             </p>
                         </div>
                     </div>
                 </div>
-                {/* /Title & Subtitle */}
             </div>
-            {/* /Text Section */}
-
-
-            
 
         </div>
     )
 }
-export default Page
+export default Page;
+
 
 
 
