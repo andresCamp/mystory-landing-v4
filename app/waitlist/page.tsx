e4ef0c758c71f9cf700dfb1d5c40689ce500bec3
andresCamp <andreslcampos@icloud.com>
2024-04-17 12:28:51 -0700

    switched sides

--- app/waitlist/page.tsx
+++ app/waitlist/page.tsx
@@ -13,6 +13,39 @@ import WaitlistForm from './WaitlistForm'
 const Page = () => {
     return (
         <div className="flex flex-col-reverse sm:flex-row min-h-screen overflow-hidden">
+
+            {/* Image Section */}
+            <div className="flex h-1/3 sm:h-full sm:w-1/2">
+                {/* Mobile nav */}
+                <div className='flex justify-between p-3 items-center absolute top-0 z-50 left-0 w-full sm:hidden'>
+                    {/* <h2 className='text-2xl w-1/3'>MyStory</h2> */}
+                    {/* <Link href={"/"} className='flex justify-center transition-all duration-500 hover:opacity-70  sm:hidden w-1/3'> */}
+                        <Image
+                            src={logo}
+                            alt="logo" 
+                            height={55}
+                        />
+                    {/* </Link> */}
+                    {/* <Link href="https://app.mystory.bio/sign-in" className='flex justify-end sm:hidden w-1/3'>
+                        <button className=' ring-2 ring-white text-white text-xl rounded-3xl px-5 py-1 transition-all duration-500 hover:bg-primary-100 hover:scale-105'>
+                            Sign In
+                        </button>
+                    </Link> */}
+                </div>
+
+                {/* Image */}
+                <div className="w-full h-full relative ">
+                    <div className="md:h-full md:w-full flex items-end"> 
+                            <Image
+                                src={img}
+                                fill
+                                placeholder="blur"
+                                alt="MyStory image holding hands"
+                                className="w-full h-full object-cover object-bottom" 
+                            />
+                    </div>
+                </div>
+            </div>
             
             {/* Text Section */}
             <div className=" flex flex-1 flex-col items-center sm:items-start justify-center bg-secondary p-8 h-2/3 sm:h-auto sm:w-1/2">
@@ -51,40 +84,7 @@ const Page = () => {
             {/* /Text Section */}
 
 
-            {/* Image Section */}
-            <div className="flex h-1/3 sm:h-full sm:w-1/2">
-                {/* Mobile nav */}
-                <div className='flex justify-between p-3 items-center absolute top-0 z-50 left-0 w-full sm:hidden'>
-                    {/* <h2 className='text-2xl w-1/3'>MyStory</h2> */}
-                    {/* <Link href={"/"} className='flex justify-center transition-all duration-500 hover:opacity-70  sm:hidden w-1/3'> */}
-                        <Image
-                            src={logo}
-                            alt="logo" 
-                            height={55}
-                        />
-                    {/* </Link> */}
-                    {/* <Link href="https://app.mystory.bio/sign-in" className='flex justify-end sm:hidden w-1/3'>
-                        <button className=' ring-2 ring-white text-white text-xl rounded-3xl px-5 py-1 transition-all duration-500 hover:bg-primary-100 hover:scale-105'>
-                            Sign In
-                        </button>
-                    </Link> */}
-                </div>
-
-                {/* Image */}
-                <div className="w-full h-full relative ">
-                    <div className="md:h-full md:w-full flex items-end"> 
-                            {/* <Image
-                                src={img}
-                                fill
-                                placeholder="blur"
-                                alt="MyStory image holding hands"
-                                className="w-full h-full object-cover object-bottom" 
-                            /> */}
-                    </div>
-                </div>
-
-             
-            </div>
+            
 
         </div>
     )
