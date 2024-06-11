5646a6551f5722ecc4b5a5ab663f373e2cd1dbc3
andresCamp <andreslcampos@icloud.com>
2024-06-10 19:26:09 -0500

    mobile root nav logo change

--- app/(welcome)/page.tsx
+++ app/(welcome)/page.tsx
@@ -3,7 +3,8 @@ import img from '@/public/mystoryimage1.png'
 import logo from '@/public/MyStoryLogo-HorizontalBlack.svg'
 import Image from 'next/image'
 import Link from 'next/link'
-import mobileLogo from '@/public/mystoryIcon.svg'
+// import mobileLogo from '@/public/mystoryIcon.svg'
+import mobileLogo from '@/public/MyStoryLogo-HorizontalWhite.svg'
 import { outfit } from '@/lib/fonts'
 
 
@@ -53,16 +54,23 @@ const Page = () => {
             <div className="flex h-1/3 sm:h-full sm:w-1/2">
                 {/* Mobile nav */}
                 <div className='flex justify-between p-3 items-center absolute top-0 z-50 left-0 w-full sm:hidden'>
-                    <h2 className='text-2xl w-1/3 text-white'>MyStory</h2>
-                    <Link href={"/"} className='flex justify-center transition-all duration-500 hover:opacity-70  sm:hidden w-1/3'>
+                    {/* <h2 className='text-2xl w-1/3 text-white'>MyStory</h2> */}
+                    <Link href={"/"} className='flex justify-start transition-all duration-500 hover:opacity-70  sm:hidden'>
                         <Image
                             src={mobileLogo}
                             alt="logo" 
                             height={55}
                         />
                     </Link>
+                    {/* <Link href={"/"} className='flex justify-center transition-all duration-500 hover:opacity-70  sm:hidden w-1/3'>
+                        <Image
+                            src={mobileLogo}
+                            alt="logo" 
+                            height={55}
+                        />
+                    </Link> */}
 
-                    <div className='flex flex-col items-end justify-end sm:hidden w-1/3'>
+                    <div className='flex flex-col items-end justify-end sm:hidden'>
                         <Link href="https://app.mystory.bio/sign-in">
                             <button className=' ring-2 ring-white text-white text-xl rounded-3xl px-5 py-1 transition-all duration-500 hover:bg-primary-100 hover:scale-105'>
                                 Sign In
