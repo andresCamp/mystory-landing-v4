4cdaa19a01d36a25edf059f96903ffcaed503f84
andresCamp <andreslcampos@icloud.com>
2024-06-10 19:37:45 -0500

    mobile nav logo

--- app/(welcome)/page.tsx
+++ app/(welcome)/page.tsx
@@ -53,9 +53,10 @@ const Page = () => {
             {/* Image Section */}
             <div className="flex h-1/3 sm:h-full sm:w-1/2">
                 {/* Mobile nav */}
-                <div className='flex justify-between p-3 items-center absolute top-0 z-50 left-0 w-full sm:hidden'>
+                <div className='flex justify-between p-3 items-center absolute top-3 z-50 left-0 w-full sm:hidden'>
                     {/* <h2 className='text-2xl w-1/3 text-white'>MyStory</h2> */}
-                    <Link href={"/"} className='flex justify-start transition-all duration-500 hover:opacity-70  sm:hidden'>
+                    {/* <Link href={"/"} className='flex justify-start transition-all duration-500 hover:opacity-70  sm:hidden'> */}
+                    <Link href={"/"} className='flex justify-start  sm:hidden'>
                         <Image
                             src={mobileLogo}
                             alt="logo" 
@@ -70,9 +71,9 @@ const Page = () => {
                         />
                     </Link> */}
 
-                    <div className='flex flex-col items-end justify-end sm:hidden'>
+                    <div className='flex flex-col items-end justify-center sm:hidden'>
                         <Link href="https://app.mystory.bio/sign-in">
-                            <button className=' ring-2 ring-white text-white text-xl rounded-3xl px-5 py-1 transition-all duration-500 hover:bg-primary-100 hover:scale-105'>
+                            <button className=' ring-2 ring-white text-white text-xl rounded-3xl px-5 py-1 transition-all duration-500 hover:bg-primary-100 hover:bg-white hover:text-black'>
                                 Sign In
                             </button>
                         </Link>
